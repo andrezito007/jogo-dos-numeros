631a5098d2bb4b936502267773a70089881d846f
andrezito <134400745+andrezito007@users.noreply.github.com>
2024-09-11 11:06:42 -0300

    Add files via upload

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -1,43 +1,25 @@
-// Verifica se a API de reconhecimento de fala está disponível
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-if (!SpeechRecognition) {
-  console.error('Speech Recognition API not supported.');
-} else {
-  const elementoChute = document.getElementById("chute");
-  
-  const recognition = new SpeechRecognition();
-  recognition.lang = 'pt-BR'; // Use 'pt-BR' para português do Brasil
-  recognition.interimResults = false; // Para não mostrar resultados intermediários
-  recognition.maxAlternatives = 1; // Para obter apenas uma alternativa
+const elementoChute = document.getElementById('chute')
+const recognition = new SpeechRecognition();
 
-  recognition.start();
+recognition.lang = 'pt-br'
+recognition.start()
 
-  // Adiciona um ouvinte de eventos para processar o resultado da fala
-  recognition.addEventListener('result', (e) => {
-    const chute = e.results[0][0].transcript;
-    exibeChuteNaTela(chute);
-  });
+recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('end', () => recognition.start())
 
-  recognition.addEventListener('error', (e) => {
-    console.error('Speech Recognition Error:', e.error);
-  });
-
-  recognition.addEventListener('end', () => {
-    console.log('Speech recognition service disconnected');
-    recognition.start(); // Reinicia o reconhecimento de fala se for interrompido
-  });
-
-  function oneSpeak(e) {
-    chute = (e.result[0] [0].transcript)
+function onSpeak(e){
+    chute = e.results[0][0].transcript
     exibeChuteNaTela(chute)
     verificaValorChuteValido(chute)
 }
 
-  function exibeChuteNaTela(chute) {
-    elementoChute.innerHTML = `
-      <div>Você disse:</div>
-      <span class="box">${chute}</span>
-    `;
-  }
+
+function exibeChuteNaTela(chute){
+  elementoChute.innerHTML = `
+  <div>Você disse:</div>
+    <span class="box"> ${chute} </span>
+  `
 }
